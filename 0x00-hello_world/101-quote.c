ec97ec491849e675ff13c72960413a650580f827
“BAISSANE” <baissanee@gmail.com>
2023-02-17 15:02:17 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 1 (Success)
  */
-
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
-write(STDERR_FILENO, msg, sizeof(msg) - 1);
-return (1);
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, msg, sizeof(msg) - 1);
+
+	return (1);
 }
