3e65d632dab96d90fcf62e62723ff54970e019ad
“BAISSANE” <baissanee@gmail.com>
2023-02-17 05:42:42 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: A program that prints the size of various computer types
- * Return 0 (Success)
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-printf("size of a char: %1u byte(s)\n", (unsigned long)sizeof(a));
-printf("size of a int: %1u byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long int: %1u byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(d));
-printf("size of a float: %1u byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeo(float));
 return (0);
 }
