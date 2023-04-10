f1fcc48960706b9ec78bcfae4370a907ae84b7c9
BAISSANE <baissanee@gmail.com>
2023-04-10 14:35:46 -0700

    Updates

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,8 @@
 
 /**
  * binary_to_uint - converts a binary number to unsigned int
- * b is pointing to a string of 0 and 1 chars
+ * @b is pointing to a string of 0 and 1 chars
+ *
  * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
